edb06c68007d8f4ef8a3e82cb61ba22bbb07ff93
Ganesh99 <36354159+SrisaiGanesh360@users.noreply.github.com>
2023-07-05 19:37:11 +0530

    050723

--- Inbuilt/fs.js
+++ Inbuilt/fs.js
@@ -1,2 +1,17 @@
 // fs stands for fiel system
 let fs = require('fs');
+
+// fs.writeFile('mycode.txt', 'We are using express', (err)=>{  // non reqritten code, fs.writeFile('mycode.txt', 'This is my node data', (err)=>{
+//     if(err) throw err;
+//     console.log('Task done');
+// })
+
+// fs.appendFile('mytext.txt', 'Code from Node FS \n',(err)=>{  // /n to print in next line 
+//     if(err) throw err;
+//     console.log('File appended');
+// })
+
+fs.readFile('mycode.txt', (err,data)=>{
+    if(err) throw err;
+    console.log(data);
+})
