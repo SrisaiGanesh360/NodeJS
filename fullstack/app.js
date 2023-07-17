45a13fb5b96d1efb13d45525ddcd21a836d8b1cd
Ganesh99 <36354159+SrisaiGanesh360@users.noreply.github.com>
2023-07-17 12:27:11 +0530

    route

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,6 +1,19 @@
-let express = require('express');
-let app = express(); // making object of express, accessing express
+let express = require ('express');
+let app = express(); // making object of express, accessing express methods
 let port = 6500;
 
 // default route
-app.get
+app.get('/',(req,res)=> {  // / is default
+    res.send('Hi From Express');
+})
+
+app.get('/test',(req,res)=> {
+    res.send('Test Route');
+})
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+    else {
+        console.log('server is running on port 6500')
+    }
+});
